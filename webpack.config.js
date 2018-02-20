083d4aa40a5f04bf1b442388d2fb76aef193cb55
Damion <damionpaulhart@gmail.com>
2018-02-20 15:51:01 -0600

    Added webpack with dev server. Project runs locally

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,14 @@
+var path = require('path');
+
 module.exports = {
-    entry: './index.js',
+    entry: './app/js/app.js',
     output: {
         filename: 'bundle.js'
+    },
+    devServer: {
+        contentBase: path.join(__dirname, "app"),
+        compress: true,
+        port: 9000
     }
 };
 
