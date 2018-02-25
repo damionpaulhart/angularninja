b6ad57dc3b215f69d4b68197ee85826a39457b20
Damion <damionpaulhart@gmail.com>
2018-02-25 07:43:36 -0600

    Final commit before adding AngularJS Material

--- app/js/app.js
+++ app/js/app.js
@@ -1,16 +1,17 @@
 'use strict';
 
-
 // Declare app level module which depends on filters, and services
 angular.module('myApp', [
-  'ngRoute',
-  'myApp.filters',
-  'myApp.services',
-  'myApp.directives',
-  'myApp.controllers'
-]).
-config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/view1', {templateUrl: 'partials/partial1.html', controller: 'MyCtrl1'});
-  $routeProvider.when('/view2', {templateUrl: 'partials/partial2.html', controller: 'MyCtrl2'});
-  $routeProvider.otherwise({redirectTo: '/view1'});
+    'ngRoute',
+    'myApp.filters',
+    'myApp.services',
+    'myApp.directives',
+    'myApp.controllers'
+]).config(['$routeProvider', function ($routeProvider) {
+    $routeProvider.when('/view1', {templateUrl: 'partials/partial1.html', controller: 'MyCtrl1'});
+    $routeProvider.when('/view2', {templateUrl: 'partials/partial2.html', controller: 'MyCtrl2'});
+    $routeProvider.when('/pageApp', {templateUrl: 'simplecalculation.html'});
+    $routeProvider.when('/simpleModule', {templateUrl: 'simplecalculation2.html', controller: 'FinanceController'});
+    $routeProvider.when('/dashboard', {templateUrl: 'dashboard.html'});
+    $routeProvider.otherwise({redirectTo: '/view1'});
 }]);
--- app/js/controllers.js
+++ app/js/controllers.js
@@ -2,10 +2,24 @@
 
 /* Controllers */
 
-angular.module('myApp.controllers', []).
-  controller('MyCtrl1', [function() {
+angular.module('myApp.controllers', [])
+    .controller('FinanceController', function ($scope) {
+        $scope.salary = 0;
+        $scope.percentage = 0;
+        $scope.result = function () {
+            return $scope.salary * $scope.percentage * 0.01;
+        };
+        $scope.remain = function () {
+            return $scope.salary - $scope.result();
+        };
+    })
+    .controller('MyCtrl1', [function () {
 
-  }])
-  .controller('MyCtrl2', [function() {
+    }])
+    .controller('MyCtrl2', [function () {
 
-  }]);
+    }])
+    .controller('GreetingController', function($scope){
+        $scope.now=new Date(); //set the model 'now' on scope
+        $scope.greeting='Hello'; //set the name model on scope
+    });
